4e9430ef1f4a062e2fbbdd838899b44ac5757ca9
pragnagopa <pgopa@microsoft.com>
2019-04-18 09:37:01 -0700

    Set up project

---
+++ Microsoft.Azure.Functions.BenchmarkWorker/Microsoft.Azure.Functions.BenchmarkWorker/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Microsoft.Azure.Functions.BenchmarkWorker
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
